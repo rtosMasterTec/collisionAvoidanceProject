5db3b255ccb4b7561fdf61cca2aaf682ed8eaf55
U-AMERICAS\hidalgoh <hidalgoh@HHIDALGO5.americas.hpqcorp.net>
2013-03-27 12:18:45 -0600

    added bot data

--- botFunctions.h
+++ botFunctions.h
@@ -1,6 +1,37 @@
 #ifndef __botFunctions__h__
 #define __botFunctions__h__
 
+// Data structures
+
+// assumed the bot size is 1 unit in length and width
+// these are the limits of the simulation space
+#define MAX_X 1200
+#define MAX_Y 800
+#define SIMULATION_TIME 100
+#define TOTAL_BOTS 100
+
+typedef location_s
+{
+   uint32_t x;
+   uint32_t y;
+
+}location_t;
+
+typedef struct trasmitData
+{
+   location_t location;
+   location_t obstacle[10];
+   uint32_t speed;
+}transmitData_t;
+
+typedef struct botData_s
+{
+   transmitData_t botData;
+}botData_t;
+
+
+uint32_t matrixData[TOTAL_BOTS][TIME];
+
 // send data to the interested listeners, position, speed, obstables, etc.
 int sendData();
 // makes the subscriptions
