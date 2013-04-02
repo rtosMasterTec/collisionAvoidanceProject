8cf8077b24990724cfdccfea07caf91b8cdfc868
Ignacio Garita <ignacio.garita@gmail.com>
2013-04-02 07:57:41 -0600

    Fixed bug inputVector.c

--- inputVector.c
+++ inputVector.c
@@ -57,54 +57,54 @@ void loadVector()
             {
                 timeInstMatrix[i][j][0] = R3;
             }
-            printf("RName %d \n", timeInstMatrix[i][j][0]);
+            printf("RName %d = %sS \n", timeInstMatrix[i][j][0], robotData.botName);
             
             ptrChar = strtok_r(NULL, " ",&ctrlPtr);
             robotData.botData.location.x = atoi(ptrChar);
             timeInstMatrix[i][j][1] = robotData.botData.location.x; 
-            printf(" RX: %d\n", robotData.botData.location.x);
+            printf(" RX: %d = %dS\n", timeInstMatrix[i][j][1], robotData.botData.location.x);
 
             ptrChar = strtok_r(NULL, " ",&ctrlPtr);
             robotData.botData.location.y = atoi(ptrChar);
             timeInstMatrix[i][j][2] = robotData.botData.location.y; 
-            printf(" RY: %d\n", robotData.botData.location.y);
+            printf(" RY: %d = %dS\n", timeInstMatrix[i][j][2], robotData.botData.location.y);
 
             ptrChar = strtok_r(NULL, " ",&ctrlPtr);
             robotData.botData.speed = atoi(ptrChar);
             timeInstMatrix[i][j][3] = robotData.botData.speed; 
-            printf(" RVel: %d\n", robotData.botData.speed);
+            printf(" RVel: %d = %dS\n", timeInstMatrix[i][j][3], robotData.botData.speed);
             
             ptrChar = strtok_r(NULL, " ",&ctrlPtr);
             strcpy(robotData.botData.direction, ptrChar);
             if(!strncmp(robotData.botData.direction, "N", 3))
             {
-                timeInstMatrix[i][j][0] = N;
+                timeInstMatrix[i][j][4] = N;
             } 
             else if(!strncmp(robotData.botData.direction, "W", 3))
             {
-                timeInstMatrix[i][j][0] = W;
+                timeInstMatrix[i][j][4] = W;
             } 
             else if(!strncmp(robotData.botData.direction, "E", 3))
             {
-                timeInstMatrix[i][j][0] = E;
+                timeInstMatrix[i][j][4] = E;
             } 
             else if(!strncmp(robotData.botData.direction, "S", 3))
             {
-                timeInstMatrix[i][j][0] = S;
+                timeInstMatrix[i][j][4] = S;
             } 
-            printf(" RDir: %s\n", robotData.botData.direction);
+            printf(" RDir: %d = %sS\n",timeInstMatrix[i][j][4], robotData.botData.direction);
 
             // Tokenize obstacle 0
             ptrChar = strtok_r(NULL, " ",&ctrlPtr);
             robotData.botData.obstacle[0] = atoi(ptrChar);
             timeInstMatrix[i][j][5] = robotData.botData.obstacle[0]; 
-            printf(" RObst0: %d\n", robotData.botData.obstacle[0]);
+            printf(" RObst0: %d = %dS\n", timeInstMatrix[i][j][5], robotData.botData.obstacle[0]);
 
             // Tokenize obstacle 1
             ptrChar = strtok_r(NULL, " ",&ctrlPtr);
             robotData.botData.obstacle[1] = atoi(ptrChar);
             timeInstMatrix[i][j][6] = robotData.botData.obstacle[1]; 
-            printf(" RObst1: %d\n", robotData.botData.obstacle[1]);
+            printf(" RObst1: %d = %dS \n", timeInstMatrix[i][j][6], robotData.botData.obstacle[1]);
 
             // Tokenize obstacle 2
             ptrChar = strtok_r(NULL, " ",&ctrlPtr);
