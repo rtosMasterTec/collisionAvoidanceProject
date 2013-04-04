76198871d342b1654e9628bd97172dad26e9c9ef
student <horacio.hidalgo@gmail.com>
2013-04-03 23:53:55 -0600

    missed to add the actual scheduler files

---
+++ scheduler.c
@@ -0,0 +1,79 @@
+#include "scheduler.h"
+
+//variables
+unsigned timeInst; // global to synchronize time accross tasks
+
+volatile pth_t pt[MAX_THREADS]; // this is volatile to not let the compiler assume anything about the value
+
+/************************************************** 
+ *
+ * Scheduler
+ *
+ ***************************************************/
+int scheduler(void)
+{
+   long numthreads;
+   int pth_ret;
+   timeInst = 0; // beggining at time 0
+   int i;
+
+   pth_ret = pth_init();
+   if (pth_ret != TRUE) 
+   {
+      fprintf(stderr, "Could not initialize GNU Pth, exiting.\n");
+      return(EXIT_FAILURE);
+   }
+   pt[0] = pth_self();
+   pt[1] = pth_spawn(PTH_ATTR_DEFAULT, mcastSubscribe, NULL);
+   pt[2] = pth_spawn(PTH_ATTR_DEFAULT, mcastUnSubscribe, NULL);
+   pt[3] = pth_spawn(PTH_ATTR_DEFAULT, receiveData, NULL);
+   pt[4] = pth_spawn(PTH_ATTR_DEFAULT, dataSelection, NULL);
+   pt[5] = pth_spawn(PTH_ATTR_DEFAULT, dataProcessing, NULL);
+
+   pt[6] = pth_spawn(PTH_ATTR_DEFAULT, decisionMaking, NULL);
+   pt[7] = pth_spawn(PTH_ATTR_DEFAULT, sendData, NULL);
+
+
+   while (1) 
+   {
+      printf("yielding\n");
+      for(i =1; i< MAX_THREADS; i++)
+      {
+         pth_yield(pt[i]);
+      }
+      numthreads = pth_ctrl(PTH_CTRL_GETTHREADS_READY);
+      if (numthreads == 0) 
+      {
+         break;
+      }
+   }
+
+   return(EXIT_SUCCESS);
+ }
+// thread examples
+// threads always yield to pt[0] as it is our scheduler
+
+void *thread_1(void *ptr)
+{
+   int i;
+   for (i = 0; i < 10; i++) {
+      printf("thread1\n  ");
+      pth_yield(pt[0]);
+   }
+
+   pth_exit(NULL);
+   return(NULL);
+}
+void *thread_2(void *ptr)
+{
+   int i;
+
+   for (i = 0; i < 10; i++) {
+      printf("thread2\n  ");
+      pth_yield(pt[0]);
+   }
+
+   pth_exit(NULL);
+   return(NULL);
+}
+
---
+++ scheduler.h
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "pth.h"
+
+#ifndef __SCHEDULER_H__
+#define __SCHEDULER_H__
+
+// variables
+extern unsigned timeInst; // global to synchronize time accross tasks
+// constants
+#define MAX_THREADS 7
+
+void *thread_1(void *ptr);
+void *thread_2(void *ptr);
+int scheduler(void);
+
+// external functions
+
+// send data to the interested listeners, position, speed, obstables, etc.
+void* sendData(void* ptr);
+// receive data from liek  position, speed, obstables, etc from members I have subscribed
+void* receiveData(void* ptr);
+
+// makes the subscriptions
+void* mcastSubscribe(void* ptr);
+
+// makes the unsubscriptions
+void* mcastUnSubscribe(void* ptr);
+
+// selects the near neighbors to subscribe to their transmitted data
+void* dataSelection(void* ptr);
+
+// process the data from selected bots
+void* dataProcessing(void* ptr);
+
+// makes decision based on processed data
+void* decisionMaking(void* ptr);
+#endif // __SCHEDULER_H__
